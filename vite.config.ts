ad6219cbeb06f28e2dd4e6778ebe15426613cfad
张伟 <zhangw@tongfun.com>
2023-02-09 16:21:10 +0800

    init:项目初始化

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    Components({
+      dts: true,
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/, /\.tsx$/], // 包含的文件类型
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+      dts: './auto-import.d.ts',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
